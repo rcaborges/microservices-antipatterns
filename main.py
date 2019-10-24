24d398940f207ffaffe9d7f505dc78f46bf32010
Rodrigo <rcaborges@gmail.com>
2019-10-24 19:58:27 +0300

    class detection

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import pandas as pd
 from network import *
 import re
 
-result = []
+imports = []
 endpoints = []
 versions = []
+classes = []
+class_calls = []
 
-def walktree(top, callback, file_type):
+def walktree(top, callback, file_type, pre_pro):
     '''recursively descend the directory tree rooted at top,
        calling the callback function for each regular file'''
 
@@ -21,52 +23,67 @@ def walktree(top, callback, file_type):
         mode = os.stat(pathname)[ST_MODE]
         if S_ISDIR(mode):
             # It's a directory, recurse into it
-            walktree(pathname, callback, file_type)
+            walktree(pathname, callback, file_type, pre_pro)
         elif S_ISREG(mode):
             # It's a file, call the callback function
-            output = callback(pathname,file_type,pathname.replace(f,""))
-            if output: result.append(output)
+            callback(pathname,file_type,pathname.replace(f,""), pre_pro)
+            #if output: result.append(output)
         else:
             # Unknown file type, print a message
             print('Skipping %s' % pathname)
 #    print([x for x in imports if x != []])
     
 
-def parsefile(filename):
+def parsefile(filename, pre_pro):
     file = open(filename, 'r')
 
-    # hard coded endpoint
     for line in file:
         if re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line):
             reg = re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line)
             for item in reg:
+                # ENDPOINTS
                 if len(item.split(".")) == 4:
                     endpoints.append([filename[filename.rfind("/")+1:],item])
+                # VERSION
                 elif len(item.split(".")) == 3:
                     versions.append([filename[filename.rfind("/")+1:],item])
 
-        #if "class" in line.split(" ") and "(" in line:
-        #    print(line)
-        #    #matchObj = re.match( r'(.*) class (.*?) .*', line, re.M|re.I)
-        #    obj = line[line.index("class")+6:line.index("(")]
-        #    #result.append([filename[filename.rfind("/")+1:],matchObj.group(2)])
-        #    result.append([filename[filename.rfind("/")+1:],obj])
+        # CLASS
+        if line.split(" ")[0] == "class" and pre_pro:
+            class_name = line.split(" ")[1][:line.lstrip().split(" ")[1].index("(")]
+            #classes.append([filename[filename.rfind("/")+1:],class_name])
+            classes.append(class_name)
+        if not pre_pro:
+            if all(item in classes for item in line.replace("("," ").replace(")"," ").split(" ")):
+                print(classes)
+                print(line)
+                input()
+                classes_calls.append([filename[filename.rfind("/")+1:],class_name])
+
+
+        # IMPORTS
         if line.split(" ")[0] == "import":
             import_name = [filename[filename.rfind("/")+1:],line.split(" ")[1].replace(";","").replace('\n','')]
-            result.append(import_name)
+            imports.append(import_name)
 
-def visitfile(file,file_type,dir):
+def visitfile(file,file_type,dir, pre_pro):
     if not file.startswith('.'):
         if file.endswith(file_type):
-            return parsefile(file)	
+            return parsefile(file, pre_pro)	
 
 if __name__ == '__main__':
-    walktree(sys.argv[1], visitfile, 'py')
+    walktree(sys.argv[1], visitfile, 'py', True)
+    print(classes)
+    input()
+    walktree(sys.argv[1], visitfile, 'py', False)
     print("ENDPOINTS")
     for endpoint in endpoints:
         print(endpoint)
     print("VERSIONS")
     for version in versions:
         print(version)
-    df = pd.DataFrame(data=result, columns=['file_name','import_name'])
+    #print("IMPORTS")
+    #for importi in imports:
+    #    print(importi)
+    df = pd.DataFrame(data=imports, columns=['file_name','import_name'])
     build_network(df, "file_name", "import_name")
