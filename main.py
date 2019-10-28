2fe98121f8bb34bf5c9f84e45fdf04feaa26e7b8
Rodrigo <rcaborges@gmail.com>
2019-10-28 14:53:19 +0200

    single and class methods

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ endpoints = []
 versions = []
 classes = []
 class_calls = []
-methods = []
-method_calls = []
+single_methods = []
+single_method_calls = []
+class_methods = []
+class_method_calls = []
+curr_class = ""
 
-forb_words = ['labels','run','name','delete','build','now','set','bom','service','options',
-              'main','fetch','value','error','write','read','filename','execute','passed',
-              'calls','git','new','next','registry','family','smc','origin','factory','ObjectType'
-              'load','failed','issues','data','deploy','repos','get','count','git_dir','metrics'
-              'compare','upstream','gradle']
+#forb_words = ['labels','run','name','delete','build','now','set','bom','service','options',
+#              'main','fetch','value','error','write','read','filename','execute','passed',
+#              'calls','git','new','next','registry','family','smc','origin','factory','ObjectType'
+#              'load','failed','issues','data','deploy','repos','get','count','git_dir','metrics'
+#              'compare','upstream','gradle']
+
+forb_words = ['run','name','delete','build','now','set','service']
 
 def walktree(top, callback, file_type, pre_pro):
     '''recursively descend the directory tree rooted at top,
@@ -45,7 +50,8 @@ def parsefile(filename, pre_pro):
     file = open(filename, 'r')
 
     for line in file:
-        if re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line):
+
+        if re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line) and not pre_pro:
             reg = re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line)
             for item in reg:
                 # ENDPOINTS
@@ -55,37 +61,38 @@ def parsefile(filename, pre_pro):
                 elif len(item.split(".")) == 3:
                     versions.append([filename[filename.rfind("/")+1:],item])
 
-#        # FUNCTION CALL
-#        if re.findall(r'[a-zA-Z]+\([^\)]*\)(\.[^\)]*\))?', line):
-#            print(line)
-#            print(re.findall(r'(?:\d{1,3}\.)+(?:\d{1,3})', line))
-#            input()
-
-#        # METHODS
-#        if line.lstrip().split(" ")[0] == "def" and pre_pro:
-#            method_name = line.lstrip().split(" ")[1][:line.lstrip().split(" ")[1].index("(")]
-#            #classes.append([filename[filename.rfind("/")+1:],class_name])
-#            methods.append(method_name)
-#        if line.lstrip().split(" ")[0] != "def" and not pre_pro and "(" in line:
-#            if [e for e in methods if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]:
-#                intersect = [e for e in methods if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]
-#                method_calls.append([filename[filename.rfind("/")+1:],intersect[0]])
-
         # CLASS
         if line.split(" ")[0] == "class" and pre_pro:
             class_name = line.split(" ")[1][:line.lstrip().split(" ")[1].index("(")]
-            #classes.append([filename[filename.rfind("/")+1:],class_name])
             classes.append(class_name)
+            #curr_class = class_name
+        # CLASS OCCURRENCE
         if line.lstrip().split(" ")[0] != "class" and not pre_pro and "(" in line:
             if [e for e in classes if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]:
                 intersect = [e for e in classes if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]
-                class_calls.append([filename[filename.rfind("/")+1:],intersect[0]])
+                for entry in intersect:
+                    class_calls.append({"file_name": filename[filename.rfind("/")+1:], "class_name": entry})
 
+        # SINGLE METHODS
+        if line.split(" ")[0] == "def" and pre_pro and re.findall(r'[a-zA-Z]+\([^\)]*\)(\.[^\)]*\))?', line):
+            method_name = line.lstrip().split(" ")[1][:line.lstrip().split(" ")[1].index("(")]
+            single_methods.append(method_name)
+        # CLASS METHODS
+        elif "def" in line.split(" ")  and pre_pro and re.findall(r'[a-zA-Z]+\([^\)]*\)(\.[^\)]*\))?', line):
+            method_name = line.lstrip().split(" ")[1][:line.lstrip().split(" ")[1].index("(")]
+            class_methods.append(method_name)
+
+        # SINGLE METHODS OCCURRENCE
+        if line.lstrip().split(" ")[0] != "def" and not pre_pro and "(" in line and re.findall(r'[a-zA-Z]+\([^\)]*\)(\.[^\)]*\))?', line):
+            if [e for e in single_methods if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]:
+                intersect = [e for e in single_methods if e in line.replace("("," ").replace(")"," ").replace("["," ").split(" ")]
+                for entry in intersect:
+                    single_method_calls.append({"file_name" : filename[filename.rfind("/")+1:], "method_name": entry})
 
         # IMPORTS
-        if line.split(" ")[0] == "import":
-            import_name = [filename[filename.rfind("/")+1:],line.split(" ")[1].replace(";","").replace('\n','')]
-            imports.append(import_name)
+        if line.split(" ")[0] == "import" and not pre_pro:
+            import_name = line.split(" ")[1].replace(";","").replace('\n','')
+            imports.append({"file_name":filename[filename.rfind("/")+1:],"import_name": import_name})
 
 def visitfile(file,file_type,dir, pre_pro):
     if not file.startswith('.'):
@@ -93,27 +100,42 @@ def visitfile(file,file_type,dir, pre_pro):
             return parsefile(file, pre_pro)	
 
 if __name__ == '__main__':
+
+    # filling class and method names 
     walktree(sys.argv[1], visitfile, 'py', True)
 
-    methods = list(set(methods))
-    methods = [x for x in methods if x not in forb_words]
+    # removing elected forbidden terms
+    smethods = list(set(single_methods))
+    print(len(smethods))
+    filt_smethods = [x for x in smethods if x not in forb_words]
+    print(len(filt_smethods))
+    print(list(set(smethods)-set(filt_smethods)))
 
+    # removing elected forbidden terms
     classes = list(set(classes))
     print(len(classes))
-    classes = [x for x in classes if x not in forb_words]
+    filt_classes = [x for x in classes if x not in forb_words]
     print(len(classes))
+    print(list(set(classes)-set(filt_classes)))
 
+    # filling methods and classes calls
     walktree(sys.argv[1], visitfile, 'py', False)
     
     print("ENDPOINTS")
-    for endpoint in endpoints:
-        print(endpoint)
-#    print("VERSIONS")
-#    for version in versions:
-#        print(version)
-#    print("IMPORTS")
-#    for importi in imports:
-#        print(importi)
+    print(len(endpoints))
+    print("VERSIONS")
+    print(len(versions))
 
-    df = pd.DataFrame(data=class_calls, columns=['file_name','class_name'])
-    build_network(df, "file_name", "class_name")
+    df_class = pd.DataFrame(data=class_calls)
+    df_smethod = pd.DataFrame(data=single_method_calls)
+    df_import = pd.DataFrame(data=imports)
+
+    #TODO
+    # check methods and classes with big names
+    # check services that have repeated entries in the df
+    # associate each class_method to its class, and check a general profile for classes
+
+    print("IMPORTS")
+    print(len(set(df_import['import_name'].values)))
+
+    #build_network(df, "file_name", "class_name")
